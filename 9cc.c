f979710805478b2771c922fc0656952bb6f29b58
S-bite <0125bitbite@gmail.com>
2019-08-09 22:45:01 +0900

    整数一つをコンパイルするコンパイラを作成

---
+++ 9cc.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(int argc,char*argv[]){
+    if (argc!=2){
+        fprintf(stderr,"引数の個数が正しくありません\n");
+        exit(1);
+    }
+    int n=atoi(argv[1]);
+    printf(".intel_syntax noprefix\n");
+    printf(".global main\n");
+    printf("main:\n");
+    printf(" mov eax, %d\n",n);
+    printf(" ret\n");
+    
+    return 0;
+}
