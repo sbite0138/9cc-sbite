b2f49ba5fe174565e1e5dcd0a10331bd4404e362
S-bite <0125bitbite@gmail.com>
2019-08-09 23:09:44 +0900

    余計な関数を削除

--- 9cc.c
+++ 9cc.c
@@ -1,22 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-/*
-文字列のポインタから数値を切り出す
-char *p="123+1";
-parse_num(p); #=> 123
-p++;
-parse_num(p); #=> 1
-*/
-int parse_num(char *p){
-    int ret=0;
-    while (*p!='\n' && ('0'<=*p && *p<='9')){
-        ret=ret*10+(*p-'0');
-        p++;
-    }
-    return ret;
-}
-
 int main(int argc,char*argv[]){
     if (argc!=2){
         fprintf(stderr,"引数の個数が正しくありません\n");
