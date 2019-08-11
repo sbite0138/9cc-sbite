01af86f9ff8ff4f32a0f40ca4df088f53b9a86f5
S-bite <0125bitbite@gmail.com>
2019-08-12 00:52:06 +0900

    error_atを追加/テストを追加

--- 9cc.c
+++ 9cc.c
@@ -22,11 +22,26 @@ struct Token
 };
 
 Token *token;
+char *user_input;
 
 void error(char *fmt, ...)
 {
     va_list ap;
     va_start(ap, fmt);
+    vfprintf(stderr, fmt, ap);
+    fprintf(stderr, "\n");
+    exit(1);
+}
+
+void error_at(char *loc, char *fmt, ...)
+{
+    va_list ap;
+    va_start(ap, fmt);
+
+    int pos = loc - user_input;
+    fprintf(stderr, "%s\n", user_input);
+    fprintf(stderr, "%*s", pos, " ");
+    fprintf(stderr, "^ ");
     vfprintf(stderr, fmt, ap);
     fprintf(stderr, "\n");
     exit(1);
@@ -46,7 +61,7 @@ void expect(char op)
 {
     if (token->kind != TK_RESERVED || token->str[0] != op)
     {
-        error("'%c'ではありません", op);
+        error_at(token->str, "'%c'ではありません", op);
     }
     token = token->next;
 }
@@ -55,7 +70,7 @@ int expect_number()
 {
     if (token->kind != TK_NUM)
     {
-        error("数ではありません");
+        error_at(token->str, "数ではありません");
     }
     int val = token->val;
     token = token->next;
@@ -100,7 +115,7 @@ Token *tokenize(char *p)
             cur->val = strtol(p, &p, 10);
             continue;
         }
-        error("トークナイズできません");
+        error_at(token->str, "トークナイズできません");
     }
     new_token(TK_EOF, cur, p);
     return head.next;
@@ -112,7 +127,7 @@ int main(int argc, char *argv[])
         fprintf(stderr, "引数の個数が正しくありません\n");
         exit(1);
     }
-
+    user_input = argv[1];
     token = tokenize(argv[1]);
 
     printf(".intel_syntax noprefix\n");
