187ab0bac8ca50616a6afb331b11585d53a2c6fb
S-bite <0125bitbite@gmail.com>
2019-08-13 22:34:09 +0900

    ノードに関する構造体と関数を作成（パーサはまだ）

--- 9cc.c
+++ 9cc.c
@@ -21,6 +21,42 @@ struct Token
     char *str;
 };
 
+typedef enum
+{
+    ND_ADD,
+    ND_SUB,
+    ND_MUL,
+    ND_DIV,
+    ND_NUM,
+} NodeKind;
+
+typedef struct Node Node;
+
+struct Node
+{
+    NodeKind kind;
+    Node *lhs;
+    Node *rhs;
+    int val; // kindがND_NUMの場合のみ使う
+};
+
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs)
+{
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = kind;
+    node->lhs = lhs;
+    node->rhs = rhs;
+    return node;
+}
+
+Node *new_node_num(int val)
+{
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = ND_NUM;
+    node->val = val;
+    return node;
+}
+
 Token *token;
 char *user_input;
 
