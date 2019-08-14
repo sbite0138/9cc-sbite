877667fd9cbbee9d90e74c40d31eb24a99f4dd2c
S-bite <0125bitbite@gmail.com>
2019-08-14 14:53:35 +0900

    単項演算子を追加

--- 9cc.c
+++ 9cc.c
@@ -159,6 +159,7 @@ Token *tokenize(char *p)
 
 Node *expr();
 Node *mul();
+Node *unary();
 Node *term();
 
 Node *expr()
@@ -184,22 +185,35 @@ Node *expr()
 Node *mul()
 {
 
-    Node *node = term();
+    Node *node = unary();
     for (;;)
     {
         if (consume('*'))
         {
-            node = new_node(ND_MUL, node, term());
+            node = new_node(ND_MUL, node, unary());
         }
         else if (consume('/'))
         {
-            node = new_node(ND_DIV, node, term());
+            node = new_node(ND_DIV, node, unary());
         }
         else
         {
             return node;
         }
     }
+}
+
+Node *unary()
+{
+    if (consume('+'))
+    {
+        return term();
+    }
+    if (consume('-'))
+    {
+        return new_node(ND_SUB, new_node_num(0), term());
+    }
+    return term();
 }
 
 Node *term()
