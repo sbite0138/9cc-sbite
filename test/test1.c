6571c9ed391a819556ed5653fa8e98bbe7331c4f
S-bite <you@example.com>
2020-11-19 19:26:25 +0900

    fix comment message

--- test/test1.c
+++ test/test1.c
@@ -1,20 +1,25 @@
 int func()
 {
+    //this is test comment
+
     int x;
-    x = -1;
+    x = -1; //this is test comment
+
     int y;
-    y = -1;
+    y = -1; //this is test comment
+
     return x - y;
 }
+//this is test comment
 
 int main()
 {
-    //hjisaasdf
-    //asdasdf
+    //this is test comment
     if (func() <= 0) {
-        printf("passed!\n");
+        printf("passed!\n"); //this is test comment
+
     } else {
-        printf("func() faild (actual:%d expect:%d)\n", func(), -1);
+        printf("func() faild (actual:%d expect:%d)\n", func(), -1); //this is test comment
     }
-    return 0;
+    return 0; //this is test comment
 }
