0e7d6f94fa0f974584ca5d7d2de3eb6441444420
sbite0138 <24842740+sbite0138@users.noreply.github.com>
2024-02-18 16:47:48 +0000

    Modified test.sh to terminate when the test fails

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
-./run.sh ./test/test0.c
-./run.sh ./test/test1.c
-./run.sh ./test/test2.c
-./run.sh ./test/test3.c
+./run.sh ./test/test0.c &&
+./run.sh ./test/test1.c &&
+./run.sh ./test/test2.c &&
+./run.sh ./test/test3.c &&
 ./run.sh ./test/test4.c
